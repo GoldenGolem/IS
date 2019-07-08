6854d996c5d9cf1395e8d21ffa759aa52ff4fb7a
Universe <luckygolden0505@gmail.com>
2019-07-07 21:54:56 -0400

    fix cors with package

--- server/headers.js
+++ server/headers.js
@@ -1,12 +1,13 @@
 module.exports = headers
-
+var cors = require('cors')
 function headers (self) {
-  self.app.use(function (req, res, next) {
-    // Add all custom system headers here
-    // Force IE to use latest rendering engine or Chrome Frame
-    res.header('X-UA-Compatible', 'IE=Edge,chrome=1');
-    res.header("Access-Control-Allow-Origin", "*");
-  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  })
+  self.app.use(cors())
+  // self.app.use(function (req, res, next) {
+  //   // Add all custom system headers here
+  //   // Force IE to use latest rendering engine or Chrome Frame
+  //   res.header('X-UA-Compatible', 'IE=Edge,chrome=1');
+  //   res.header("Access-Control-Allow-Origin", "*");
+  // 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //   next();
+  // })
 }
