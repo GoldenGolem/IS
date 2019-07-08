d377b5eddacd108c9c1bbf467077c5ab971a7ce4
Universe <luckygolden0505@gmail.com>
2019-07-07 22:01:43 -0400

    fix for port

--- server/headers.js
+++ server/headers.js
@@ -1,7 +1,10 @@
 module.exports = headers
 var cors = require('cors')
 function headers (self) {
-  self.app.use(cors())
+  self.app.use(cors({
+  	credentials: true,
+  	origin: 'http://3.16.161.67:3000'
+  }))
   // self.app.use(function (req, res, next) {
   //   // Add all custom system headers here
   //   // Force IE to use latest rendering engine or Chrome Frame
@@ -11,3 +14,4 @@ function headers (self) {
   //   next();
   // })
 }
+	
