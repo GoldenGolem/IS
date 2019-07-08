6e4b5aa52aaf06e196f9879e290a061d92d58fe7
Universe <luckygolden0505@gmail.com>
2019-07-07 21:51:41 -0400

    fix cors issue

--- server/headers.js
+++ server/headers.js
@@ -4,7 +4,9 @@ function headers (self) {
   self.app.use(function (req, res, next) {
     // Add all custom system headers here
     // Force IE to use latest rendering engine or Chrome Frame
-    res.header('X-UA-Compatible', 'IE=Edge,chrome=1')
-    next()
+    res.header('X-UA-Compatible', 'IE=Edge,chrome=1');
+    res.header("Access-Control-Allow-Origin", "*");
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
   })
 }
