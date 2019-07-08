de935174bafe65cb91b3270a4ee17d3f384bcea6
Universe <luckygolden0505@gmail.com>
2019-07-07 22:03:29 -0400

    return back

--- server/headers.js
+++ server/headers.js
@@ -1,17 +1,15 @@
 module.exports = headers
 var cors = require('cors')
 function headers (self) {
-  self.app.use(cors({
-  	credentials: true,
-  	origin: 'http://3.16.161.67:3000'
-  }))
-  // self.app.use(function (req, res, next) {
-  //   // Add all custom system headers here
-  //   // Force IE to use latest rendering engine or Chrome Frame
-  //   res.header('X-UA-Compatible', 'IE=Edge,chrome=1');
-  //   res.header("Access-Control-Allow-Origin", "*");
-  // 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  //   next();
-  // })
+  // self.app.use(cors({
+  // 	credentials: true,
+  // 	origin: 'http://3.16.161.67:3000	'
+  // }))
+  self.app.use(function (req, res, next) {
+    // Add all custom system headers here
+    // Force IE to use latest rendering engine or Chrome Frame
+    res.header('X-UA-Compatible', 'IE=Edge,chrome=1');
+    next();
+  })
 }
 	
